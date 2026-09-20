d2d7d6edd400b0d7c3e6e7a5125f058c76d3abc1
Rafael Ferreira <rafaelmonteiro72006@gmail.com>
2026-09-19 22:26:51 -0300

    test: update TaskItemTest and remove TaskNoteTest

--- tests/Domain.UnitTest/Entities/TaskItemTests.cs
+++ tests/Domain.UnitTest/Entities/TaskItemTests.cs
@@ -7,12 +7,12 @@ namespace Domain.UnitTest.Entities;
 public class TaskItemTests
 {
     private readonly TaskItem _taskItem = new(
-        Guid.NewGuid(),
-        "Título original",
-        "Descrição original",
-        new DateTime(2026, 9, 10),
-        PriorityLevel.Low);
-
+            Guid.NewGuid(),
+            "Título original",
+            "Descrição original",
+            new DateTime(2026, 9, 10),
+            PriorityLevel.Low);
+    
     [Fact]
     public void Constructor_ShouldInitializeTaskProperties()
     {
@@ -35,35 +35,8 @@ public class TaskItemTests
         Assert.Equal(userId, task.UserId);
         Assert.Equal(title, task.Title);
         Assert.Equal(description, task.Description);
-        Assert.False(task.Status == Status.Pending);
+        Assert.Equal(Status.Pending, task.Status);
         Assert.Equal(dueDate.Date, task.DueDate);
-        Assert.Equal(priority, task.Priority);
-    }
-
-    [Fact]
-    public void Constructor_ShouldSetDueDateToNull_WhenDueDateIsNull()
-    {
-        // Arrange
-        var userId = Guid.NewGuid();
-        var title = "Título";
-        var description = "Descrição";
-        DateTime? dueDate = null;
-        var priority = PriorityLevel.High;
-
-        // Act
-        var task = new TaskItem(
-            userId,
-            title,
-            description,
-            dueDate?.Date,
-            priority);
-
-        // Assert
-        Assert.Equal(userId, task.UserId);
-        Assert.Equal(title, task.Title);
-        Assert.Equal(description, task.Description);
-        Assert.False(task.Status == Status.Pending);
-        Assert.Null(task.DueDate);
         Assert.Equal(priority, task.Priority);
     }
     
@@ -106,7 +79,7 @@ public class TaskItemTests
         Assert.Equal(newDueDate.Date, _taskItem.DueDate);
         Assert.Equal(newPriority, _taskItem.Priority);
     }
-
+    
     [Fact]
     public void Update_ShouldSetDueDateToNull()
     {
@@ -120,7 +93,7 @@ public class TaskItemTests
         // Assert
         Assert.Null(_taskItem.DueDate);
     }
-
+    
     [Fact]
     public void Update_ShouldThrowDomainException_WhenPriorityIsInvalid()
     {
@@ -139,12 +112,147 @@ public class TaskItemTests
     }
 
     [Fact]
-    public void Complete_ShouldMarkTaskAsCompleted()
+    public void AddNote_ShouldSetNoteProperties()
     {
+        // Arrange
+        var noteText = "NewNote";
+
+        // Act
+        var taskNote = _taskItem.AddNote(noteText);
+
+        // Assert
+        Assert.NotNull(taskNote);
+        Assert.Equal(noteText, taskNote.Note);
+    }
+
+    [Fact]
+    public void UpdateNote_ShouldUpdateNoteText()
+    {
+        // Arrange
+        var currentNote = "Current Note";
+        
+        var taskNote = _taskItem.AddNote(currentNote);
+        
+        var updateNoteText = "Update Note";
+        
+        // Act
+        _taskItem.UpdateNote(taskNote.Id,updateNoteText);
+        
+        // Assert
+        Assert.Equal(updateNoteText, taskNote.Note);
+    }
+
+    [Fact]
+    public void UpdateNote_ShouldThrowNotFoundException_WhenNoteDoesNotExist()
+    {
+        // Arrange
+        var invalidNoteId = 999;
+
+        // Act
+        var action = () =>
+            _taskItem.UpdateNote(invalidNoteId, "Updated Note");
+
+        // Assert
+        Assert.Throws<NotFoundException>(action);
+    }
+
+    [Fact]
+    public void RemoveNote_ShouldRemoveNote()
+    {
+        // Arrange
+        var taskNote = _taskItem.AddNote("Note to remove");
+
+        // Act
+        _taskItem.RemoveNote(taskNote.Id);
+
+        // Assert
+        Assert.DoesNotContain(taskNote, _taskItem.Notes);
+    }
+    
+    [Fact]
+    public void RemoveNote_ShouldThrowNotFoundException_WhenNoteDoesNotExist()
+    {
+        // Arrange
+        var invalidNoteId = 999;
+
+        // Act
+        var action = () => _taskItem.RemoveNote(invalidNoteId);
+
+        // Assert
+        Assert.Throws<NotFoundException>(action);
+    }
+    
+    [Fact]
+    public void InProgress_ShouldSetStatusToInProgress()
+    {
+        // Act
+        _taskItem.InProgress();
+        
+        // Assert
+        Assert.Equal(Status.InProgress, _taskItem.Status);
+    }
+
+    [Fact]
+    public void Complete_ShouldSetStatusToCompleted()
+    {
+        // Arrange
+        _taskItem.InProgress();
+        
         // Act
         _taskItem.Complete();
         
         // Assert
-        Assert.True(_taskItem.Status == Status.Completed);
+        Assert.Equal(Status.Completed, _taskItem.Status);
+    }
+
+    [Fact]
+    public void Complete_ShouldThrowDomainException_WhenStatusIsPending()
+    {
+        // Act
+        var action = () => _taskItem.Complete();
+        
+        //Assert
+        Assert.Throws<DomainException>(action);
+    }
+
+    [Fact]
+    public void Complete_ShouldThrowDomainException_WhenThereArePendingNotes()
+    {
+        // Arrange
+        _taskItem.InProgress();
+
+        _taskItem.AddNote("Note");
+        
+        // Act
+        var action = () => _taskItem.Complete();
+        
+        // Assert
+        Assert.Throws<DomainException>(action);
+    }
+
+    [Fact]
+    public void MarkAsDone_ShouldSetDoneStatus()
+    {
+        // Arrange
+        var taskNote = _taskItem.AddNote("Note");
+        
+        // Act
+        _taskItem.MarkNoteAsDone(taskNote.Id);
+        
+        // Assert
+        Assert.True(taskNote.Done);
+    }
+    
+    [Fact]
+    public void MarkAsUndone_ShouldSetUndoneStatus()
+    {
+        // Arrange
+        var taskNote = _taskItem.AddNote("Note");
+        
+        // Act
+        _taskItem.MarkNoteAsUndone(taskNote.Id);
+        
+        // Assert
+        Assert.False(taskNote.Done);
     }
 }
